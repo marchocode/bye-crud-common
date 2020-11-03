85d2d65c54d017c72a0e30dee8d0be0379fbde00
MRC <1763907575@qq.com>
2020-11-03 10:02:30 +0800

    update 1.3.1 add exception global and assents

--- src/main/java/xyz/chaobei/common/exception/Asserts.java
+++ src/main/java/xyz/chaobei/common/exception/Asserts.java
@@ -8,13 +8,19 @@ public class Asserts {
      * <p>author: <a href='mailto:maruichao52@gmail.com'>MRC</a>
      *
      * @param message 提示语
-     * @return void
      * @since 2020/10/15
      **/
     public static void fail(String message) {
         throw new ApiException(message);
     }
 
+    /**
+     * <p>断言抛出一个异常
+     * <p>author: <a href='mailto:maruichao52@gmail.com'>MRC</a>
+     *
+     * @param iErrorCode 异常code与message
+     * @since 2020/10/15
+     **/
     public static void fail(IErrorCode iErrorCode) {
         throw new ApiException(iErrorCode);
     }
